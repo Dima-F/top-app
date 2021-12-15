673e904428b2d41e1cefb7810f96cb4d157d9688
Dima Fiialo <dafw@live.ru>
2021-12-16 00:38:56 +0200

    htag component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import { AppProps } from 'next/dist/shared/lib/router/router'
 import '../styles/globals.css'
+import React from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>MyTop - наш лучший топ</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
